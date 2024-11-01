f65ab237d78fc8f085c8bdd2a2b36443b3d5fc73
charmaineGoh <23021210@myrp.edu.sg>
2024-11-01 14:27:27 +0800

    updated

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{useState} from 'react';
+import {TextInput, Text, View, Button, Alert, TouchableOpacity, ToastAndroid, } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const InputBox=({label,onChangeText})=>{
+    return (
+        <View>
+           <Text>{label}</Text>
+            <TextInput style={{borderWidth:1}} onChangetext={onChangeText}/>
+
+        </View>
+    );
+};
+
+
+
+const MyApp = () => {
+    const [pw, setPw] = useState('');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    return (
+        <View style={{padding: 20, paddingTop: 50}}>
+
+            {/*Exercise 1B*/}
+            <Text>User Type:</Text>
+            <RNPickerSelect onValueChange={(value) => setPw(value)}
+                            items={[
+                                {label: 'Admin', value: 'Admin'},
+                                {label: 'Guest', value: 'Guest'},
+                                {label: 'User', value: 'User'},
+                            ]}
+            />
+
+            {/*Exercise 1G*/}
+            <InputBox label="User Name:" onChangeText={(text) => setName(text)}/>
+            <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+            <TouchableOpacity onPress={() =>
+                ToastAndroid.show('Welcome' + type + '' + name, ToastAndroid.SHORT)}>
+                <Text>Log in</Text>
+            </TouchableOpacity>
+            <Text>{pw}</Text>
+        </View>
+    );
+
+
+    {/* Exercise 1A */}
+    <Text>Password:</Text>
+    <TextInput
+        style={{ borderWidth: 1 }}
+    />
+
+
+
+          {/*Exercise 1C & D*/}
+          <Button
+              title="Submit Answers"
+              onPress={()=>Alert.alert("meow")}
+              />
+
+          {/*Exercise 1E*/}
+          <TouchableOpacity
+              onPress={()=>Alert.alert("Welcome")}>
+              <Text>Submit Answers</Text>
+          </TouchableOpacity>
+
+          {/*Exercise 1F*/}
+          <TouchableOpacity
+              onPress={()=>ToastAndroid.show(ToastAndroid.SHORT)}>
+              <Text>lOG IN</Text>
+              </TouchableOpacity>
+
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyApp;
+
+
+
+
+
